778a06ed85659c2baae0f903e4a9f4d1384856cb
Martin Gencur <mgencur@redhat.com>
2016-02-25 14:36:02 +0100

    Use Arquillian-chameleon

--- server/integration/testsuite/src/test/java/org/infinispan/server/test/client/hotrod/HotRodRemoteCacheIT.java
+++ server/integration/testsuite/src/test/java/org/infinispan/server/test/client/hotrod/HotRodRemoteCacheIT.java
@@ -10,7 +10,6 @@ import org.infinispan.server.test.category.HotRodClustered;
 import org.infinispan.server.test.category.HotRodLocal;
 import org.infinispan.server.test.category.Smoke;
 import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.container.test.api.OverProtocol;
 import org.jboss.arquillian.container.test.api.TargetsContainer;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
@@ -40,56 +39,48 @@ public class HotRodRemoteCacheIT extends AbstractRemoteCacheIT {
 
     @Deployment(testable = false, name = "filter-1")
     @TargetsContainer("container1")
-    @OverProtocol("jmx-as7")
     public static Archive<?> deployFilter1() {
         return createFilterArchive();
     }
 
     @Deployment(testable = false, name = "converter-1")
     @TargetsContainer("container1")
-    @OverProtocol("jmx-as7")
     public static Archive<?> deployConverter1() {
         return createConverterArchive();
     }
 
     @Deployment(testable = false, name = "filter-converter-1")
     @TargetsContainer("container1")
-    @OverProtocol("jmx-as7")
     public static Archive<?> deployFilterConverter1() {
         return createFilterConverterArchive();
     }
 
     @Deployment(testable = false, name = "converter-2")
     @TargetsContainer("container2")
-    @OverProtocol("jmx-as7")
     public static Archive<?> deployConverter2() {
         return createConverterArchive();
     }
 
     @Deployment(testable = false, name = "filter-2")
     @TargetsContainer("container2")
-    @OverProtocol("jmx-as7")
     public static Archive<?> deployFilter2() {
         return createFilterArchive();
     }
 
     @Deployment(testable = false, name = "filter-converter-2")
     @TargetsContainer("container2")
-    @OverProtocol("jmx-as7")
     public static Archive<?> deployFilterConverter2() {
         return createFilterConverterArchive();
     }
 
     @Deployment(testable = false, name = "key-value-filter-converter-1")
     @TargetsContainer("container1")
-    @OverProtocol("jmx-as7")
     public static Archive<?> deployKeyValueFilterConverter1() {
       return createKeyValueFilterConverterArchive();
    }
 
     @Deployment(testable = false, name = "key-value-filter-converter-2")
     @TargetsContainer("container2")
-    @OverProtocol("jmx-as7")
     public static Archive<?> deployKeyValueFilterConverter2() {
       return createKeyValueFilterConverterArchive();
    }
